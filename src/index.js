0a3dc27cea4ce5e68110ca8028287323e0ba6610
Justin Hopkins <justin@commoncode.io>
2019-05-01 17:56:38 +1000

    Created my CC react app

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import Root from "./containers/Root";
+import configureStore from "./store/configureStore";
+import * as serviceWorker from "./serviceWorker";
+
+export const store = configureStore();
+
+ReactDOM.render(
+  <Router>
+    <Root store={store} />
+  </Router>,
+  document.getElementById("root"),
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+// Learn more about service workers: http://bit.ly/CRA-PWA
 serviceWorker.unregister();
